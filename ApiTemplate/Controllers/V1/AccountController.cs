738a7f60294358f301ffe12a55cd9d261b6e18cc
Mahammad Agayev <mahammad.agayev@simbrella.com>
2021-09-23 20:49:06 +0400

    Added account controller and validation

---
+++ ApiTemplate/Controllers/V1/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using IdentityServer.Abstarct;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using StorageCore.Domain.Abstract;
+
+namespace ApiTemplate.Controllers.V1
+{
+
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AccountController(IAccountService accountService, IUnitOfWork unitOfWork)
+        {
+            _accountService = accountService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Authenticate(PhoneNumberAuthenticateRequest request)
+        {
+            var resp = await _accountService.Authenticate(request);
+
+            return Ok(resp);
+        }
+    }
+}
---
+++ ApiTemplate/Validators/PhoneNumberAuthenticateRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using IdentityServer.Models;
+
+namespace ApiTemplate.Validators
+{
+    public class PhoneNumberAuthenticateRequestValidator : AbstractValidator<PhoneNumberAuthenticateRequest>
+    {
+        public PhoneNumberAuthenticateRequestValidator()
+        {
+            this.RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number cannot be empty");
+            this.RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty");
+        }
+    }
+}
